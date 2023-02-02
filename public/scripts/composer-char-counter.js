1b3a62f84b54cac45d543d8991579734e0a5dbc8
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-02-01 19:27:03 -0800

    new tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+console.log("program ready")
+
+$(".text-area").on('input', function() {
+
+  const section = $(this).closest(".div-btn-counter");
+
+  const counter = section.find(".counter");
+  //console.log(counter)
+
+  const maxChars = 140;
+  //console.log($(this).val());
+  
+  const remainingText = maxChars - $(this).val().length;
+
+  if(remainingText < 0){
+    $('.counter').css({
+      'color': 'red'
+    })
+  } else{
+    $('.counter').css({
+      'color': '#545149'
+
+  })
+}
+  
+  //console.log(remainingText);
+  $('.counter').val(remainingText);
+  
+})
+
+
+});
